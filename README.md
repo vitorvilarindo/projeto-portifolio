f6b38c41b80b01761c160c02e1d588d0c6a298e8
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2023-07-01 11:02:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-portifolio
+ Meu portifolio
---
+++ menu.js
@@ -0,0 +1,10 @@
+const itens = document.querySelector('.cabeçalho-menu-links')
+        function clickMenu(){
+            if (itens.style.display == 'block'){
+                itens.style.display = 'none'
+            }
+            else {
+                itens.style.display = 'block'
+            }
+
+        }
